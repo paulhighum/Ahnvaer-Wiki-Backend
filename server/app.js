dda52049f2c8700479f6088292dfd014003781d3
Paul Highum <phigh07@gmail.com>
2017-12-04 11:58:48 -0700

    Creates backend initial data, deploys to heroku, adds express and cors

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors")
+const port = process.env.PORT || 3000
+const data = require("./data/data.js")
+
+const app = express()
+app.use(cors())
+
+app.get("/", (request, response) => {
+  response.json(data)
+})
+
+app.listen(port, () => {
+  console.log("listening", port)
+})
---
+++ server/data/data.js
@@ -0,0 +1,58 @@
+const races = [
+  {
+    id: 1,
+    name: "Thallarite",
+    description: "",
+    "common geography": {
+      region: "",
+      cities: ""
+    }
+  },{
+    id: 2,
+    name: "Garethan",
+    description: "",
+    "common geography": {
+      region: "",
+      cities: ""
+    }
+  },{
+    id: 3,
+    name: "Fyrenian",
+    description: "",
+    "common geography": {
+      region: "",
+      cities: ""
+    }
+  },{
+    id: 4,
+    name: "Pelvoshi",
+    description: "",
+    "common geography": {
+      region: "",
+      cities: ""
+    }
+  },{
+    id: 5,
+    name: "Lurthen",
+    description: "",
+    "common geography": {
+      region: "",
+      cities: ""
+    }
+  },{
+    id: 6,
+    name: "Faltaron",
+    description: "",
+    "common geography": {
+      region: "",
+      cities: ""
+    }
+  }
+]
+
+const cities = ["Ovoule",]
+
+module.exports = {
+  races,
+  cities
+}
