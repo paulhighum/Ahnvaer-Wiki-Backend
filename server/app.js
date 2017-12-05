440a20ad09c0efb94b27cce60f3e01815826458b
Paul Highum <phigh07@gmail.com>
2017-12-05 14:10:05 -0700

    Refactors addRegionsToRaces function

--- server/app.js
+++ server/app.js
@@ -2,20 +2,22 @@ const express = require("express")
 const cors = require("cors")
 const port = process.env.PORT || 3000
 const data = require("./data/data.js")
+let races = data.races
+let regions = data.regions
 let updatedRaces = {}
 
 const app = express()
 app.use(cors())
 
-function addRegionsToRaces(data){
+function addRegionsToRaces(races, regions){
   updatedRaces = data.races.map((race) => {
     race.geography.regions = []
     race.geography.cities.forEach(city => {
-      for(var i=0; i<data.regions.length; i++){
-        for(var j=0; j<data.regions[i].cities.length; j++){
-          if(city === data.regions[i].cities[j]){
-            if(race.geography.regions.includes(data.regions[i].name)===false) {
-              race.geography.regions.push(data.regions[i].name)
+      for(var i=0; i<regions.length; i++){
+        for(var j=0; j<regions[i].cities.length; j++){
+          if(city === regions[i].cities[j]){
+            if(race.geography.regions.includes(regions[i].name)===false) {
+              race.geography.regions.push(regions[i].name)
             }
           }
         }
@@ -25,7 +27,11 @@ function addRegionsToRaces(data){
   })
   return updatedRaces
 }
-addRegionsToRaces(data)
+addRegionsToRaces(races, regions)
+
+module.exports = {
+  addRegionsToRaces
+}
 
 
 app.get("/", (request, response) => {
---
+++ test/app.test.js
@@ -0,0 +1,92 @@
+const assert = require("assert")
+const app = require("../app")
+
+
+const inputArray = [
+  {
+    "id": 1,
+    "name": "Thallarite",
+    "description": "",
+    "common geography": {
+      "cities": [
+        "Corsaya",
+        "Enia"
+      ],
+      "regions": [
+        "Angareel"
+      ]
+    }
+  },
+  {
+    "id": 2,
+    "name": "Garethan",
+    "description": "",
+    "common geography": {
+      "cities": [
+        "Caprifar",
+        "Knovaya City",
+        "Barosh's Hold",
+        "Cerrar",
+        "Ovoule"
+      ],
+      "regions": [
+        "Grasulahn",
+        "The Siphaer Delta"
+      ]
+    }
+  },
+  {
+    "id": 3,
+    "name": "Fyrenian",
+    "description": "",
+    "common geography": {
+      "cities": [
+        "Ovoule",
+        "Jalowna",
+        "Suhnberg",
+        "Melmejin",
+        "Tobbindru"
+      ],
+      "regions": [
+        "The Siphaer Delta",
+        "The Zeeran Desert"
+      ]
+    }
+  },
+  {
+    "id": 4,
+    "name": "Pelvoshi",
+    "description": "",
+    "common geography": {
+      "cities": [
+        ""
+      ],
+      "regions": []
+    }
+  },
+  {
+    "id": 5,
+    "name": "Lurthen",
+    "description": "",
+    "common geography": {
+      "cities": [
+        ""
+      ],
+      "regions": []
+    }
+  },
+  {
+    "id": 6,
+    "name": "Faltaron",
+    "description": "",
+    "common geography": {
+      "cities": [
+        "Ahndaril",
+        "Valint"
+      ],
+      "regions": [
+        "Alahnvaer"
+      ]
+    }
+  }
+]
