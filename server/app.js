c282d08de5d93a515120b2f487b57b03c344fbb7
Paul Highum <phigh07@gmail.com>
2017-12-05 13:42:36 -0700

    Adds data transformation to server side code

--- server/app.js
+++ server/app.js
@@ -2,12 +2,34 @@ const express = require("express")
 const cors = require("cors")
 const port = process.env.PORT || 3000
 const data = require("./data/data.js")
+let updatedRaces = {}
 
 const app = express()
 app.use(cors())
 
+function addRegionsToRaces(data){
+  updatedRaces = data.races.map((race) => {
+    race["common geography"].regions = []
+    race["common geography"].cities.forEach(city => {
+      for(var i=0; i<data.regions.length; i++){
+        for(var j=0; j<data.regions[i].cities.length; j++){
+          if(city === data.regions[i].cities[j]){
+            if(race["common geography"].regions.includes(data.regions[i].name)===false) {
+              race["common geography"].regions.push(data.regions[i].name)
+            }
+          }
+        }
+      }
+    })
+    return race
+  })
+  return updatedRaces
+}
+addRegionsToRaces(data)
+
+
 app.get("/", (request, response) => {
-  response.json(data)
+  response.json(updatedRaces)
 })
 
 app.listen(port)
--- server/data/data.js
+++ server/data/data.js
@@ -4,55 +4,79 @@ const races = [
     name: "Thallarite",
     description: "",
     "common geography": {
-      region: "",
-      cities: ""
+      cities: ["Corsaya", "Enia"]
     }
   },{
     id: 2,
     name: "Garethan",
     description: "",
     "common geography": {
-      region: "",
-      cities: ""
+      cities: ["Caprifar", "Knovaya City", "Barosh's Hold", "Cerrar", "Ovoule"]
     }
   },{
     id: 3,
     name: "Fyrenian",
     description: "",
     "common geography": {
-      region: "",
-      cities: ""
+      cities: ["Ovoule", "Jalowna", "Suhnberg", "Melmejin", "Tobbindru"]
     }
   },{
     id: 4,
     name: "Pelvoshi",
     description: "",
     "common geography": {
-      region: "",
-      cities: ""
+      cities: [""]
     }
   },{
     id: 5,
     name: "Lurthen",
     description: "",
     "common geography": {
-      region: "",
-      cities: ""
+      cities: [""]
     }
   },{
     id: 6,
     name: "Faltaron",
     description: "",
     "common geography": {
-      region: "",
-      cities: ""
+      cities: ["Ahndaril", "Valint"]
     }
   }
 ]
 
-const cities = ["Ovoule",]
+const regions = [
+  {
+    "region-id": 1,
+    name: "Angareel",
+    cities: ["Corsaya", "Enia"]
+  },{
+    "region-id": 2,
+    name: "Grasulahn",
+    cities: ["Caprifar", "Knovaya City", "Barosh's Hold"]
+  },{
+    "region-id": 3,
+    name: "The Siphaer Delta",
+    cities: ["Cerrar", "Ovoule"]
+  },{
+    "region-id": 4,
+    name: "The Zeeran Desert",
+    cities: ["Jalowna", "Suhnberg", "Melmejin", "Tobbindru"]
+  },{
+    "region-id": 5,
+    name: "Khopeshka",
+    cities: []
+  },{
+    "region-id": 6,
+    name: "Chaos Coast",
+    cities: []
+  },{
+    "region-id": 7,
+    name: "Alahnvaer",
+    cities: ["Ahndaril", "Valint"]
+  }
+]
 
 module.exports = {
   races,
-  cities
+  regions,
 }
