9d77fecb8f2bd9b5be1e1439119af4f25e38c796
Paul Highum <phigh07@gmail.com>
2017-12-06 14:05:01 -0700

    Refactoring of data set, removing unused id numbers

--- server/data/data.js
+++ server/data/data.js
@@ -1,41 +1,35 @@
 const races = [
   {
-    id: 1,
     name: "Thallarite",
     description: "",
     geography: {
       cities: ["Corsaya", "Enia"]
     }
   },{
-    id: 2,
     name: "Garethan",
     description: "",
     geography: {
       cities: ["Caprifar", "Knovaya City", "Barosh's Hold", "Cerrar", "Ovoule"]
     }
   },{
-    id: 3,
     name: "Fyrenian",
     description: "",
     geography: {
       cities: ["Ovoule", "Jalowna", "Suhnberg", "Melmejin", "Tobbindru"]
     }
   },{
-    id: 4,
     name: "Pelvoshi",
     description: "",
     geography: {
       cities: [""]
     }
   },{
-    id: 5,
     name: "Lurthen",
     description: "",
     geography: {
       cities: [""]
     }
   },{
-    id: 6,
     name: "Faltarron",
     description: "",
     geography: {
@@ -46,31 +40,24 @@ const races = [
 
 const regions = [
   {
-    "region-id": 1,
     name: "Angareel",
     cities: ["Corsaya", "Enia"]
   },{
-    "region-id": 2,
     name: "Grasulahn",
     cities: ["Caprifar", "Knovaya City", "Barosh's Hold"]
   },{
-    "region-id": 3,
     name: "The Siphaer Delta",
     cities: ["Cerrar", "Ovoule"]
   },{
-    "region-id": 4,
     name: "The Zeeran Desert",
     cities: ["Jalowna", "Suhnberg", "Melmejin", "Tobbindru"]
   },{
-    "region-id": 5,
     name: "Khopeshka",
     cities: []
   },{
-    "region-id": 6,
     name: "Chaos Coast",
     cities: []
   },{
-    "region-id": 7,
     name: "Alahnvaer",
     cities: ["Ahndaril", "Valint"]
   }
