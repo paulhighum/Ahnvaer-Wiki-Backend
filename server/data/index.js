55476c07b15d815a5a72d272cff8b1e6ec4df37c
Paul Highum <phigh07@gmail.com>
2018-01-08 14:03:26 -0700

    Extracts methods

--- server/data/index.js
+++ server/data/index.js
@@ -3,58 +3,55 @@ let races = data.races
 let regions = data.regions
 let updatedRaces = []
 
-function addRegionsToRaces(races, regions){
-  return updatedRaces = races.map((race) => {
-    race.geography.regions = []
-    race.geography.cities.forEach(city => {
-      for(var i=0; i<regions.length; i++){
-        for(var j=0; j<regions[i].cities.length; j++){
-          if(city === regions[i].cities[j]){
-            if(race.geography.regions.includes(regions[i].name)===false) {
-              race.geography.regions.push(regions[i].name)
-            }
-          }
-        }
-      }
-    })
-    return race
-  })
-}
-addRegionsToRaces(races, regions)
-
 // function addRegionsToRaces(races, regions){
-//   addsRegionArrayToRaces(races)
-// }
-// addRegionsToRaces(races, regions)
-//
-// function addsRegionArrayToRaces(races){
 //   return updatedRaces = races.map((race) => {
 //     race.geography.regions = []
-//     matchesCitiesWithRegions(races, regions)
+//     race.geography.cities.forEach(city => {
+//       for(var i=0; i<regions.length; i++){
+//         for(var j=0; j<regions[i].cities.length; j++){
+//           if(city === regions[i].cities[j]){
+//             if(race.geography.regions.includes(regions[i].name)===false) {
+//               race.geography.regions.push(regions[i].name)
+//             }
+//           }
+//         }
+//       }
+//     })
 //     return race
 //   })
 // }
-//
-// function matchesCitiesWithRegions(races, regions){
-//   race.geography.cities.forEach(city => {
-//     regionsLoop(regions)
-//   })
-// }
-//
-// function regionsLoop(regions){
-//   for(var i=0; regions.length; i++){
-//     citiesLoop(regions[i].cities)
-//   }
-// }
-//
-// function citiesLoop(cities){
-//   for(var j=0; j<cities.length; j++){
-//     if(race.geography.regions.includes(regions[i].name)===false) {
-//       race.geography.regions.push(regions[i].name)
-//     }
-//   }
-// }
+// addRegionsToRaces(races, regions)
+
+function matchRaceRegions(races) {
+  return races.map(race => {
+    const cities = race.geography.cities
+    let regions = findRegionForCities(cities)
+    race.geography.regions = makeUnique(regions)
+    return race
+  })
+}
+
+function findRegion(city) {
+  let regionMatch = regions.find(region => {
+    return region.cities.includes(city)
+  })
+  return regionMatch ? regionMatch.name : null
+}
+
+function findRegionForCities(cities) {
+  return cities.map(findRegion).filter(val => val)
+}
+
+function makeUnique(arr){
+  return Array.from(new Set(arr))
+}
+
+updatedRaces= matchRaceRegions(races)
 
 module.exports = {
-  races: updatedRaces
+  races: updatedRaces,
+  matchRaceRegions,
+  findRegion,
+  findRegionForCities,
+  makeUnique,
 }
--- test/app.test.js
+++ test/app.test.js
@@ -1,81 +1,8 @@
 const assert = require("assert")
-const app = require("../server/app.js")
+const dataTools = require("../server/data")
 const data = require("../server/data/data.js")
 const inputRaces = data.races
 const inputRegions = data.regions
-
-const racesWithEmptyRegionArrays = [
-  {
-    "name": "Thallarite",
-    "description": "Thallarites are a small statured race, the tallest of which stand about as tall as the smallest of humans. Their ears are huge in comparison to the rest of their bodies and heads, and stick out as opposed to lying flat as human ears do. Their hair is often thicker and fuller than human hair. Their eyes, though, come in mostly the same shades and colors as those of human eyes. What truly sets Thallarites apart, however, are their feet. Their feet are nearly as agile as their hands, allowing them to grab things and pick them up as easily as a human does with their hands, or to walk along ropes or trees as easily as a human walks on the ground.   ",
-    "geography": {
-      "cities": [
-        "Corsaya",
-        "Enia"
-      ],
-      "regions": []
-    }
-  },
-  {
-    "name": "Garethan",
-    "description": "Garethans, as a race, are enormous, towering at heights of up to eight feet. But they are more than just tall; they are large, with thickset muscular bodies that hold a strength that can crush boulders and bullish large heads with broad flat faces. Their skin does not soften their appearance for it ranges in color from a stoic gray to an impenetrable black and is so rough and leathery that most airborne projectiles fail to pierce it. Yet it is neither their size nor their strength that most physically distinguishes them, but their head and back spikes. These spikes protrude over eight inches and a constant line of them begin at their forehead, go up over the crown of their skull, and start down their spine before fading away. The spikes are more than just fearsome in appearance, they are deadly, capable of impaling a man with a head-butt",
-    "geography": {
-      "cities": [
-        "Caprifar",
-        "Knovaya City",
-        "Barosh's Hold",
-        "Cerrar",
-        "Ovoule"
-      ],
-      "regions": []
-    }
-  },
-  {
-    "name": "Fyrenian",
-    "description": "",
-    "geography": {
-      "cities": [
-        "Ovoule",
-        "Jalowna",
-        "Suhnberg",
-        "Melmejin",
-        "Tobbindru"
-      ],
-      "regions": []
-    }
-  },
-  {
-    "name": "Pelvoshi",
-    "description": "",
-    "geography": {
-      "cities": [
-        ""
-      ],
-      "regions": []
-    }
-  },
-  {
-    "name": "Lurthen",
-    "description": "",
-    "geography": {
-      "cities": [
-        ""
-      ],
-      "regions": []
-    }
-  },
-  {
-    "name": "Faltarron",
-    "description": "",
-    "geography": {
-      "cities": [
-        "Ahndaril",
-        "Valint"
-      ],
-      "regions": []
-    }
-  }
-]
 
 const outPutArray = [
   {
@@ -163,13 +90,7 @@ const outPutArray = [
 describe("App", function () {
   describe("#addRegionsToRaces", function () {
     it("Adds a regions array to each race", function (){
-      assert.deepEqual(app.addsRegionArrayToRaces(inputRaces), racesWithEmptyRegionArrays)
-    })
-    it("There are no repeat region names in any race's regions array", function () {
-      assert.equal()
-    })
-    it("Adds region names based on which cities a race is found in", function (){
-      assert.deepEqual()
+      assert.deepEqual(dataTools.matchRaceRegions(inputRaces), outPutArray)
     })
   })
 })
